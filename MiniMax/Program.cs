10dc2abc1e1ca2f50a4c48a6e54bf82a86849615
Eric Aquilotti <ERIC.AQUILOTTI@local.ittcesena.it>
2021-10-01 09:31:45 +0200

    V1.1: aggiunta calcolo minimo

--- MiniMax/Program.cs
+++ MiniMax/Program.cs
@@ -10,6 +10,17 @@ namespace MiniMax
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5] { 3, 8, -12, 20, -3 };
+
+            int min = array[0];
+            for(int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            Console.WriteLine(min);
+
+            Console.ReadKey();
         }
     }
 }
